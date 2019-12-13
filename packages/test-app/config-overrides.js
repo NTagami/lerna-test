ce63a11b772a35503e153de3898e6107f2a8a9d0
Naoki Tagami <Naoki.Tagami@sony.com>
2019-12-13 17:27:08 +0900

    feat: add pcd loader

--- packages/test-app/config-overrides.js
+++ packages/test-app/config-overrides.js
@@ -1,2 +1,25 @@
-const { useBabelRc, override } = require('customize-cra');
-module.exports = override(useBabelRc());
+const { tap, useBabelRc, override, overrideDevServer } = require('customize-cra');
+
+/*headers: {
+	    "Access-Control-Allow-Methods": "*",
+	    'Access-Control-Allow-Headers': '*',
+	    "Access-Control-Allow-Origin": "*"
+	},
+*/
+
+function myConfig(config){
+    return {
+	proxy: {
+	    '/pcd': {
+		target:"https://github.com",
+		pathRewrite:{'^/pcd':''}
+	    }
+	},
+	...config
+    };
+}
+
+module.exports = {
+    webpack: override(useBabelRc()),
+    devServer: overrideDevServer()
+};
--- packages/test-app/src/ThreeTest.tsx
+++ packages/test-app/src/ThreeTest.tsx
@@ -1,11 +1,27 @@
+/** @jsx jsx */
 import * as React from "react";
 import { DispatchProp } from "react-redux";
+
+import { jsx, css } from "@emotion/core";
 
 import { useButtonHelper } from "./CommonStyles";
 import { push } from "connected-react-router";
 import * as Three from "three";
-import { Canvas, useFrame, ReactThreeFiber } from "react-three-fiber";
+import {
+  Canvas,
+  useFrame,
+  useLoader,
+  ReactThreeFiber
+} from "react-three-fiber";
 
+import { PCDLoader } from "three/examples/jsm/loaders/PCDLoader";
+
+const theme = css`
+  width: 50vw;
+  height: 50vh;
+  background-color: #000;
+`;
+/*
 type mesh = ReactThreeFiber.Object3DNode<Three.Mesh, typeof Three.Mesh>;
 type boxBufferGeometry = ReactThreeFiber.BufferGeometryNode<
   Three.BoxBufferGeometry,
@@ -15,11 +31,12 @@ type meshNormalMaterial = ReactThreeFiber.MaterialNode<
   Three.MeshNormalMaterial,
   [Three.MeshNormalMaterialParameters]
 >;
-
+*/
 interface Node {
   rotation?: Three.Euler;
 }
 
+//const Thing = React.memo(() => {
 const Thing: React.FC = () => {
   const ref = React.useRef<Node>(null);
 
@@ -44,13 +61,76 @@ const Thing: React.FC = () => {
   );
 };
 
+const Test = React.memo(() => {
+  //const Test: React.FC = () => {
+  const ref = React.useRef<Node>(null);
+
+  useFrame(() => {
+    const current: Node | null = ref.current;
+
+    if (current != null && current.rotation != null) {
+      current.rotation.y += 0.01;
+    }
+  });
+
+  const points = useLoader(
+    PCDLoader,
+    "/Zaghetto.pcd"
+    //"http://localhost:3000/pcd/mrdoob/three.js/raw/dev/examples/models/pcd/binary/Zaghetto.pcd"
+    //"/pcd/mrdoob/three.js/raw/dev/examples/models/pcd/binary/Zaghetto.pcd"
+    //"https://github.com/mrdoob/three.js/raw/dev/examples/models/pcd/binary/Zaghetto.pcd"
+    //"http://github.com/mrdoob/three.js/raw/dev/examples/models/pcd/binary/Zaghetto.pcd"
+  );
+  //React.Suspense
+
+  return (
+    <mesh ref={ref} position={[0, 0, 3]}>
+      <arrowHelper
+        attach="dir"
+        args={[new Three.Vector3(0, 1, 0)]}
+      ></arrowHelper>
+      <arrowHelper
+        attach="dir"
+        args={[new Three.Vector3(1, 0, 0)]}
+      ></arrowHelper>
+      <arrowHelper
+        attach="dir"
+        args={[new Three.Vector3(0, 0, 1)]}
+      ></arrowHelper>
+      <mesh scale={[6, -6, 6]} position={[-2, 3.2, -3]}>
+        <primitive object={points} />
+      </mesh>
+    </mesh>
+  );
+  //  return <div></div>;
+});
+
 export const ThreeTest: React.FC<DispatchProp> = ({ dispatch }) => {
   const button = useButtonHelper();
   return (
     <>
-      <Canvas>
-        <Thing />
-      </Canvas>
+      <div css={theme}>
+        <Canvas
+          onCreated={({ gl, camera }) => {
+            camera.lookAt(new Three.Vector3(0, 1, 0));
+
+            /*
+            gl.clippingPlanes.push(
+              new Three.Plane(new Three.Vector3(0, -1, 0), 0)
+            );*/
+          }}
+          camera={{
+            fov: 75,
+            position: [0, -0.5, 5],
+            near: 0.1,
+            far: 1000
+          }}
+        >
+          <React.Suspense fallback={null}>
+            <Test />
+          </React.Suspense>
+        </Canvas>
+      </div>
       <div>{button("Main", () => dispatch(push("/")))}</div>
     </>
   );
